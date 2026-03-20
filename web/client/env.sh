88aafd3f704ad98511c9c1544ba9698174780f55
Johann Medina <63032152+johannmx@users.noreply.github.com>
2026-03-20 18:38:58 -0300

    fix: use explicit echo in env.sh to ensure variable expansion

--- web/client/env.sh
+++ web/client/env.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-cat <<EOF > /usr/share/nginx/html/env-config.js
-window._env_ = {
-  VITE_API_URL: "${VITE_API_URL}",
-}
-EOF
+echo "window._env_ = {" > /usr/share/nginx/html/env-config.js
+echo "  VITE_API_URL: \"${VITE_API_URL}\"," >> /usr/share/nginx/html/env-config.js
+echo "}" >> /usr/share/nginx/html/env-config.js
 exec "$@"
