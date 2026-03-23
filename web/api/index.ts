f39c0b36be230667e3a006d0ba0baadadcb623fd
Johann Medina <63032152+johannmx@users.noreply.github.com>
2026-03-23 11:31:34 -0300

    fix: add trust proxy setting for express-rate-limit

--- web/api/index.ts
+++ web/api/index.ts
@@ -6,6 +6,7 @@ import helmet from 'helmet';
 import { rateLimit } from 'express-rate-limit';
 
 const app = express();
+app.set('trust proxy', true);
 app.disable('x-powered-by');
 const PORT = process.env.PORT || 3001;
 const DATA_DIR = 'data';
